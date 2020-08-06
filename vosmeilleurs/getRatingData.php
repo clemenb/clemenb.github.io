babfcf8626d74874d856f8099a4173f4699ee340
clemenb <53978832+clemenb@users.noreply.github.com>
2020-08-07 00:52:53 +0100

    new

--- vosmeilleurs/getRatingData.php
+++ vosmeilleurs/getRatingData.php
@@ -1,41 +1,42 @@
 <?php
-require_once "db.php";
-require_once "functions.php";
+namespace Phppot;
+
+use Phppot\Rating;
+require_once "./Common/Config.php";
+$config = new Config();
+require_once "./Model/Rating.php";
+$rating = new Rating();
 // Here the user id is harcoded.
 // You can integrate your authentication code here to get the logged in user id
-$userId = 1;
+$userId = 5;
 
-$query = "SELECT * FROM tbl_restaurant ORDER BY id DESC";
-$result = mysqli_query($conn, $query);
+$apperance = $config::RATING_APPEARANCE;
 
-$outputString = '';
+$courseResult = $rating->getCourse();
+if (! empty($courseResult)) {
+    foreach ($courseResult as $row) {
+        $userRating = $rating->getUserRating($userId, $row['id']);
+        $totalRating = $rating->getTotalRating($row['id']);
+        $date = date_create($row["last_date_to_register"]);
+        ?>
+<div class="row-item">
+    <div class="row-title"><?php echo $row['name']; ?></div>
+    <ul class="list-inline" id="list-<?php echo $row['id']; ?>">
+ <?php require $apperance . "-rating-view.php"; ?>
 
-foreach ($result as $row) {
-    $userRating = userRating($userId, $row['id'], $conn);
-    $totalRating = totalRating($row['id'], $conn);
-    $outputString .= '
-        <div class="row-item">
- <div class="row-title">' . $row['name'] . '</div> <div class="response" id="response-' . $row['id'] . '"></div>
- <ul class="list-inline"  onMouseLeave="mouseOutRating(' . $row['id'] . ',' . $userRating . ');"> ';
-    
-    for ($count = 1; $count <= 5; $count ++) {
-        $starRatingId = $row['id'] . '_' . $count;
-        
-        if ($count <= $userRating) {
-            
-            $outputString .= '<li value="' . $count . '" id="' . $starRatingId . '" class="star selected">&#9733;</li>';
-        } else {
-            $outputString .= '<li value="' . $count . '"  id="' . $starRatingId . '" class="star" onclick="addRating(' . $row['id'] . ',' . $count . ');" onMouseOver="mouseOverRating(' . $row['id'] . ',' . $count . ');">&#9733;</li>';
-        }
-    } // endFor
-    
-    $outputString .= '
- </ul>
- 
- <p class="review-note">Total Reviews: ' . $totalRating . '</p>
- <p class="text-address">' . $row["address"] . '</p>
+        <img src="img/loader.gif" class="loader-icon" id="loader-icon">
+    </ul>
+    <div class="response" id="response-<?php echo $row['id']; ?>"></div>
+
+
+    <p class="review-note">Total Reviews: <?php echo $totalRating; ?></p>
+    <p class="text-address">
+        <label class="course-detail">Period: <?php echo $row["period"]; ?></label><label
+            class="course-detail">Available seats: <?php echo $row["availabe_seats"]; ?></label><label
+            class="course-detail">Last Date to Register: <?php echo date_format($date, "d M Y"); ?></label>
+    </p>
 </div>
- ';
+<?php
+    }
 }
-echo $outputString;
 ?>
