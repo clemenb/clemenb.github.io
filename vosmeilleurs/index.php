fc5b32ecdb3fe5d75fe95fdde695ddc5301c9045
clemenb <53978832+clemenb@users.noreply.github.com>
2020-08-06 18:30:56 +0100

    update

--- vosmeilleurs/index.php
+++ vosmeilleurs/index.php
@@ -1,6 +1,7 @@
 <?php if($_POST["message"]) {
     mail("info@vosmeilleurs.com", "Notify Me!", $_POST["message"], "From: an@email.address");}?>
-<!DOCTYPE html><html lang="en">
+<!DOCTYPE html>
+<html lang="en">
   <head>
     <meta http-equiv="content-type" content="text/html; charset=utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
